c19ca05a594f2a9673c58dbed42b1e53e4f9564d
Robert Gadbois <rgadbois@netflix.com>
2018-03-13 09:51:46 -0700

    fix 64 bit compile errors

--- src/ideviceinstaller.c
+++ src/ideviceinstaller.c
@@ -907,7 +907,7 @@ run_again:
 					dstpath = NULL;
 
 					zip_uint64_t zfsize = 0;
-					while (zfsize < zs.size) {
+					while (zfsize < (zip_uint64_t)zs.size) {
 						zip_int64_t amount = zip_fread(zfile, buf, sizeof(buf));
 						if (amount == 0) {
 							break;
@@ -925,7 +925,7 @@ run_again:
 								total += written;
 							}
 							if (total != amount) {
-								fprintf(stderr, "Error: wrote only %d of %" PRIi64 "\n", total, amount);
+								fprintf(stderr, "Error: wrote only %d of %zd" PRIi64 "\n", total, amount);
 								afc_file_close(afc, af);
 								zip_fclose(zfile);
 								free(dstpath);
